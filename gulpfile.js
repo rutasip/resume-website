d2a1df26a8c2c6cb4e2cc238fbd4f32fee0c4fed
Ruta Sipkauskaite <ruta.sipkauskaite@gmail.com>
2019-12-30 22:26:05 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -143,7 +143,7 @@ function watchFiles() {
 
 // Deploy to gh pages
 
-gulp.task("deploy", function() {
+gulp.task("deploy", ["build"], function() {
   return gulp.src("./dist/**/*").pipe(deploy());
 });
 
